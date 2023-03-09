6d3ddea7379524d74643e26503d6f77295f4de42
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-03-09 16:22:22 +0100

    feat: (8) add id property and add examine function to treasure class

--- src/classes/treasure.ts
+++ src/classes/treasure.ts
@@ -3,12 +3,14 @@ import { Player } from "./player"
 
 // Exemple de trésor pour la quatrième salle
 export class Treasure implements IInteractiveObject {
+  id: number
   name: string
   description: string
   weight: number
   inventory: IInteractiveObject[]
 
-  constructor(description: string, weight: number = 0 , inventory: IInteractiveObject[] = [] ) {
+  constructor(id: number, description: string, weight: number = 0 , inventory: IInteractiveObject[] = [] ) {
+    this.id = id
     this.name = "Trésor"
     this.description = description
     this.inventory = inventory
@@ -16,7 +18,7 @@ export class Treasure implements IInteractiveObject {
   }
 
   examine() {
-    return this.description
+    return `Id :  ${this.id} \n Name : ${this.name} : \n Description : ${this.description} \n Inventory: ${this.inventory} Weight : ${this.weight} !`
   }
 
   use(player : Player) {
