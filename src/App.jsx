69452f96871f95dc7e20d17027dee1aa56b81e2d
Bryan Neveu <bryan@pando-studio.com>
2023-04-07 17:58:51 +0200

    add : init css

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,167 @@
-import "./App.css"
-import Controllers from "./components/Controllers.tsx"
-import { Room } from "./classes/room.ts"
-import { Armor } from "./classes/takables/armor.ts"
-import { Weapon } from "./classes/takables/weapon.ts"
-import { Player } from "./classes/player.ts"
-import armorsJson from "./data/armor.json"
-import weaponsJson from "./data/weapon.json"
-import roomsJson from "./data/room.json"
-import trapsJson from "./data/trap.json"
-import treasuresJson from "./data/treasure.json"
-import riddlesJson from "./data/riddle.json"
-import playersJson from "./data/player.json"
-import monstersJson from "./data/monster.json"
-import { Monster } from "./classes/monster.ts"
-import { Riddle } from "./classes/riddle.ts"
-import { Trap } from "./classes/trap.ts"
-import { Treasure } from "./classes/takables/treasure.ts"
-import { useState } from "react"
+import './App.css'
+import Controllers from './components/Controllers.tsx'
+import { Room } from './classes/room.ts'
+import { Armor } from './classes/takables/armor.ts'
+import { Weapon } from './classes/takables/weapon.ts'
+import { Player } from './classes/player.ts'
+import armorsJson from './data/armor.json'
+import weaponsJson from './data/weapon.json'
+import roomsJson from './data/room.json'
+import trapsJson from './data/trap.json'
+import treasuresJson from './data/treasure.json'
+import riddlesJson from './data/riddle.json'
+import playersJson from './data/player.json'
+import monstersJson from './data/monster.json'
+import { Monster } from './classes/monster.ts'
+import { Riddle } from './classes/riddle.ts'
+import { Trap } from './classes/trap.ts'
+import { Treasure } from './classes/takables/treasure.ts'
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [roomIndex, setRoomIndex] = useState(0)
+  const incrementRoomIndex = () => setRoomIndex(el => ++el)
 
-	const [roomIndex, setRoomIndex] = useState(0)
-	const incrementRoomIndex = () => setRoomIndex(el => ++el)
+  //
+  const armorsData = []
+  const riddles = []
+  const weaponsData = []
+  const rooms = []
+  const players = []
+  const monsters = []
+  const traps = []
+  const treasures = []
 
-	//
-	const armorsData = []
-	const riddles = []
-	const weaponsData = []
-	const rooms = []
-	const players = []
- 	const monsters = []
-	const traps = []
-	const treasures = []
+  const getInventory = object => {
+    switch (object.type) {
+      case 'weapons':
+        return weaponsData[object.id]
+        break
+      case 'armors':
+        return armorsData[object.id]
+        break
+      case 'monster':
+        return monsters[object.id]
+        break
+      case 'trap':
+        return traps[object.id]
+        break
+      case 'riddle':
+        return riddles[object.id]
+        break
+      case 'treasure':
+        return treasures[object.id]
+        break
+      case 'player':
+        return players[object.id]
+        break
+      default:
+        return {}
+        break
+    }
+  }
 
-	const getInventory = (object) => {
-		switch (object.type) {
-			case "weapons":
-				return weaponsData[object.id]
-				break
-			case "armors":
-				return armorsData[object.id]
-				break
-			case "monster":
-				return monsters[object.id]
-				break
-			case "trap":
-				return traps[object.id]
-				break
-			case "riddle":
-				return riddles[object.id]
-				break
-			case "treasure":
-				return treasures[object.id]
-				break
-			case "player":
-				return players[object.id]
-				break
-			default:
-				return {}
-				break
-		}
-	}
+  // Instantiate Rooms
 
-	// Instantiate Rooms
-	
-	armorsJson.armor.map(armor => armorsData.push(new Armor(armor.id, armor.name, armor.description, armor.protection, armor.weight)))
+  armorsJson.armor.map(armor =>
+    armorsData.push(
+      new Armor(
+        armor.id,
+        armor.name,
+        armor.description,
+        armor.protection,
+        armor.weight
+      )
+    )
+  )
 
-	// Instantiate Weapons
-	weaponsJson.weapon.map(weapon => weaponsData.push(new Weapon(weapon.id, weapon.name, weapon.description, weapon.damage, weapon.weight)))
+  // Instantiate Weapons
+  weaponsJson.weapon.map(weapon =>
+    weaponsData.push(
+      new Weapon(
+        weapon.id,
+        weapon.name,
+        weapon.description,
+        weapon.damage,
+        weapon.weight
+      )
+    )
+  )
 
-	riddlesJson.riddle.map(riddle => riddles.push( new Riddle(riddle.id, riddle.question, riddle.answer, riddle.weight, riddle.reward)))
+  riddlesJson.riddle.map(riddle =>
+    riddles.push(
+      new Riddle(
+        riddle.id,
+        riddle.question,
+        riddle.answer,
+        riddle.weight,
+        riddle.reward
+      )
+    )
+  )
 
-	trapsJson.trap.map(trap => traps.push( new Trap(trap.id, trap.description, trap.damage, trap.weight)))
-	
-	treasuresJson.treasure.map(treasure => treasures.push(new Treasure(treasure.id, treasure.name, treasure.description, treasure.weight, treasure.inventory.map(el => getInventory(el)))))
-	
-	monstersJson.monster.map(monster => monsters.push(new Monster(monster.id, monster.name, monster.currentLP, monster.weight, monster.description, [...monster.inventory.map(el => getInventory(el))])))
-	
-	playersJson.player.map(player => players.push(new Player(player.id,player.type, player.name, rooms[0], player.currentLP, player.weight, [...player.inventory.map(el => getInventory(el))])))
+  trapsJson.trap.map(trap =>
+    traps.push(new Trap(trap.id, trap.description, trap.damage, trap.weight))
+  )
 
-	roomsJson.room.map(room => rooms.push(new Room(room.id, room.name, room.description, [...room.objects.map(el => getInventory(el))])))
+  treasuresJson.treasure.map(treasure =>
+    treasures.push(
+      new Treasure(
+        treasure.id,
+        treasure.name,
+        treasure.description,
+        treasure.weight,
+        treasure.inventory.map(el => getInventory(el))
+      )
+    )
+  )
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<Controllers room = { rooms[roomIndex] } player={players[0]} nextRoom={rooms[roomIndex + 1]} setRoomIndex={incrementRoomIndex} endGame={() => setRoomIndex(9)}></Controllers>
-			</header>
-		</div>
-	)
+  monstersJson.monster.map(monster =>
+    monsters.push(
+      new Monster(
+        monster.id,
+        monster.name,
+        monster.currentLP,
+        monster.weight,
+        monster.description,
+        [...monster.inventory.map(el => getInventory(el))]
+      )
+    )
+  )
+
+  playersJson.player.map(player =>
+    players.push(
+      new Player(
+        player.id,
+        player.type,
+        player.name,
+        rooms[0],
+        player.currentLP,
+        player.weight,
+        [...player.inventory.map(el => getInventory(el))]
+      )
+    )
+  )
+
+  roomsJson.room.map(room =>
+    rooms.push(
+      new Room(room.id, room.name, room.description, [
+        ...room.objects.map(el => getInventory(el))
+      ])
+    )
+  )
+
+  return (
+    <div className='App'>
+      <Controllers
+        room={rooms[roomIndex]}
+        player={players[0]}
+        nextRoom={rooms[roomIndex + 1]}
+        setRoomIndex={incrementRoomIndex}
+        endGame={() => setRoomIndex(9)}
+      ></Controllers>
+    </div>
+  )
 }
 
 export default App
--- src/components/Controllers.tsx
+++ src/components/Controllers.tsx
@@ -1,73 +1,88 @@
-import React, { useState } from "react"
-import { Room } from "../classes"
+import React, { useState } from 'react'
+import { Room } from '../classes'
 
 const Controllers = ({
-	room,
-	setRoomIndex,
-	endGame,
+  room,
+  setRoomIndex,
+  endGame
 }: {
-	room: Room
-	setRoomIndex: Function
-	endGame: Function
+  room: Room
+  setRoomIndex: Function
+  endGame: Function
 }): JSX.Element => {
-	const [response, setResponse] = useState("")
-	const [player, setPlayer] = useState({})
+  const [response, setResponse] = useState('')
+  const [player, setPlayer] = useState({})
 
-	const testRiddle = (obj) => {
-		const go = obj.answer === response
-		if (go) {
-			setRoomIndex()
-		} else {
-			player.setCurrentLP(player.currentLP - 10)
-			if (player.currentLP <= 0) {
-				endGame()
-			}
-		}
-		setResponse("")
-	}
+  const testRiddle = obj => {
+    const go = obj.answer === response
+    if (go) {
+      setRoomIndex()
+    } else {
+      player.setCurrentLP(player.currentLP - 10)
+      if (player.currentLP <= 0) {
+        endGame()
+      }
+    }
+    setResponse('')
+  }
 
-	const selectPlayer = (id) => {
-		setPlayer(room.objects.find((el) => el.id === id))
-		setRoomIndex()
-	}
+  const selectPlayer = id => {
+    setPlayer(room.objects.find(el => el.id === id))
+    setRoomIndex()
+  }
 
-	const interactBoolean = () => {
-		setRoomIndex()
-	}
+  const interactBoolean = () => {
+    setRoomIndex()
+  }
 
-	return (
-		<>
-			<p>{`Points de vie:  ${player.currentLP ?? 0}`}</p>
-			<p>{room.getDescription()}</p>
+  return (
+    <>
+      {room.id !== 0 && room.id !== 1 && room.id !== 9 && (
+        <div className='lifepoint__container'>
+          <div className='lifepoint__heart'>
+            <p className='lifepoint__number'>{player.currentLP ?? 0}</p>
+          </div>
+        </div>
+      )}
 
-			{room.id === 0 && (
-				<button onClick={() => setRoomIndex()}> Démarrer </button>
-			)}
-			{room.id === 1 && (
-				<p>
-					{room.objects.map((player) => (
-						<>
-							<button onClick={() => selectPlayer(player.id)}>
-								{player.name}
-							</button>
-						</>
-					))}
-				</p>
-			)}
-			{room.objects.map((obj: any, key: number) => (
-				<>
-					{obj.inputType === "prompt" && (
-						<>
-							<p>{obj.question}</p>
-							<input
-								value={response}
-								onKeyDown={(e) => (e.code === "Enter" ? testRiddle(obj) : null)}
-								onChange={(e) => setResponse(e.target.value)}
-							/>
-							<button onClick={(e) => testRiddle(obj)}> Valider </button>
-						</>
-					)}
-					{/* {obj.inputType === "boolean" && (
+      <p>{room.getDescription()}</p>
+
+      {room.id === 0 && (
+        <button onClick={() => setRoomIndex()} className='button'>
+          Démarrer
+        </button>
+      )}
+      {room.id === 1 && (
+        <p>
+          {room.objects.map(player => (
+            <>
+              <button
+                onClick={() => selectPlayer(player.id)}
+                className='button'
+              >
+                {player.name}
+              </button>
+            </>
+          ))}
+        </p>
+      )}
+      {room.objects.map((obj: any, key: number) => (
+        <>
+          {obj.inputType === 'prompt' && (
+            <>
+              <p>{obj.question}</p>
+              <input
+                value={response}
+                onKeyDown={e => (e.code === 'Enter' ? testRiddle(obj) : null)}
+                onChange={e => setResponse(e.target.value)}
+              />
+              <button onClick={e => testRiddle(obj)} className='button'>
+                {' '}
+                Valider{' '}
+              </button>
+            </>
+          )}
+          {/* {obj.inputType === "boolean" && (
 						<div>
 							<p>{obj.description}</p>
 							{obj.name === "Piège" && <p>{obj.name}</p>}
@@ -75,31 +90,31 @@ const Controllers = ({
 							<button onClick={interactBoolean}>Joueur 2</button>
 						</div>
 					)} */}
-					{/* {obj.inputType === "multiple" && (
+          {/* {obj.inputType === "multiple" && (
 						<>
 							{obj?.choices.map((el: string) => (
 								<button onClick={interactBoolean}>{el}</button>
 							))}
 						</>
 					)} */}
-				</>
-			))}
+        </>
+      ))}
 
-			{/*  room de fin */}
-			{room.id === 9 &&
-				(player.currentLP <= 0 ? (
-					<>
-						<p>T'es mort</p>
-						<a href="/">Rejouer</a>
-					</>
-				) : (
-					<>
-						<p> Bravo champion</p>
-						<a href="/">Rejouer</a>
-					</>
-				))}
-		</>
-	)
+      {/* ENDING ROOM */}
+      {room.id === 9 &&
+        (player.currentLP <= 0 ? (
+          <>
+            <p>T'es mort</p>
+            <a href='/'>Rejouer</a>
+          </>
+        ) : (
+          <>
+            <p> Bravo champion</p>
+            <a href='/'>Rejouer</a>
+          </>
+        ))}
+    </>
+  )
 }
 
 export default Controllers
